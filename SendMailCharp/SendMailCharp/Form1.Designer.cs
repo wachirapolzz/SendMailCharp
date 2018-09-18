5e5e2c16d91e5a336bc032c289a59c8e69e5729d
wachirapolzz <43362927+wachirapolzz@users.noreply.github.com>
2018-09-18 11:22:48 +0700

    Email Form

---
+++ SendMailCharp/SendMailCharp/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SendMailCharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendMail = new System.Windows.Forms.Button();
+            this.from = new System.Windows.Forms.TextBox();
+            this.to = new System.Windows.Forms.TextBox();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.body = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sendMail
+            // 
+            this.sendMail.Location = new System.Drawing.Point(197, 226);
+            this.sendMail.Name = "sendMail";
+            this.sendMail.Size = new System.Drawing.Size(75, 23);
+            this.sendMail.TabIndex = 0;
+            this.sendMail.Text = "SendMail";
+            this.sendMail.UseVisualStyleBackColor = true;
+            this.sendMail.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // from
+            // 
+            this.from.Location = new System.Drawing.Point(56, 29);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(203, 20);
+            this.from.TabIndex = 1;
+            this.from.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // to
+            // 
+            this.to.Location = new System.Drawing.Point(56, 76);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(203, 20);
+            this.to.TabIndex = 2;
+            this.to.TextChanged += new System.EventHandler(this.to_TextChanged);
+            // 
+            // subject
+            // 
+            this.subject.Location = new System.Drawing.Point(56, 122);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(203, 20);
+            this.subject.TabIndex = 3;
+            // 
+            // body
+            // 
+            this.body.Location = new System.Drawing.Point(56, 166);
+            this.body.Multiline = true;
+            this.body.Name = "body";
+            this.body.Size = new System.Drawing.Size(203, 54);
+            this.body.TabIndex = 4;
+            this.body.TextChanged += new System.EventHandler(this.body_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "From";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "To";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subject";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Body";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.body);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.sendMail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendMail;
+        private System.Windows.Forms.TextBox from;
+        private System.Windows.Forms.TextBox to;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.TextBox body;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ SendMailCharp/SendMailCharp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace SendMailCharp
+{
+    public partial class Form1 : Form
+    {
+        private MailMessage myMail = new MailMessage();
+        private SmtpClient smtpClient = new SmtpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.myMail.Priority = MailPriority.High;
+            this.myMail.BodyEncoding = System.Text.Encoding.UTF8;
+            this.myMail.IsBodyHtml = true;
+            this.smtpClient.Host = "tarmg.cpall.co.th";
+            this.smtpClient.Port = 25;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string from = this.from.Text;
+            string to = this.to.Text;
+            string subject = this.subject.Text;
+            string body = this.body.Text;
+
+            try
+            {
+
+                myMail.To.Clear();
+                myMail.CC.Clear();
+
+
+            //    myMail.From = new MailAddress("ทดสอบแจ้งเตือน WorkFlow Error K2<noreply@cpall.co.th>");
+
+                myMail.From = new MailAddress(from);
+                myMail.To.Add(to);
+
+
+
+                myMail.Subject = subject;
+
+                myMail.Body = body;
+
+
+                this.smtpClient.Send(myMail);
+
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void to_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void body_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+   
+    }
+}
